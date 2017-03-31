34e31ef3edfb786a87bd60b312cae7866b01845b
Andrzej Fedorowicz <andrzej.fedorowicz18@alumni.loyola.ca>
2017-03-31 14:46:55 -0400

    February 31

--- JS/myScript.js
+++ JS/myScript.js
@@ -3,6 +3,21 @@ function TextBox(){
 	document.getElementById('inp').focus();
 }
 
-function Submit(){
-	document.getElementById("password").submit()
+/*Function is onclick at the submit button on the login button*/
+function login(){
+	var Username = "Andrzej Fedorowicz"
+	/*setting variable Username as my name. This will be the variable the code will check in order to "log you in"*/
+	var Password = "Password"
+	/*Password variable is set to "Password". The function will also check the Password form to check if the input is the same*/
+	if (document.getElementById("username").value === Username) {
+		/*if statement checks the username form input. If it matches, the function continues. If not, there will be an alert 
+		saying an incorrect username was submitted.*/
+		if (document.getElementById("password").value === Password) {
+			/*If statement checks if the password input matches the Password variable. If so, the second header on the webpage will 
+			change to say "Welcome Username(variable)" Otherwise, there will be an alert saying an incorrect password was inputed.*/
+			document.getElementById("welcome").innerHTML = "Welcome " + Username;
+		} else {alert("Icorrect Password")}
+	} else {alert("Incorrect Username")}
 }
+
+/*function ends*/
